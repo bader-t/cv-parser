28029d2bb0e0f14fafc55da8fc6af1f07e69dc6d
Bader Toumi <55552093+bader-t@users.noreply.github.com>
2022-07-28 19:34:13 +0100

    added logs

--- src/main/java/com/example/ocrtest/services/CVParserServiceImpl.java
+++ src/main/java/com/example/ocrtest/services/CVParserServiceImpl.java
@@ -1,11 +1,13 @@
 package com.example.ocrtest.services;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.text.PDFTextStripper;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 @Service
+@Slf4j
 public class CVParserServiceImpl implements CVParserService{
 
     @Override
