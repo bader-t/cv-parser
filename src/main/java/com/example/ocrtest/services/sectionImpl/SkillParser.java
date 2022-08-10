8eae1c3a754b12984409f0d009d167b967c8d65e
marouane <marwanezibout10@gmail.com>
2022-08-10 11:49:21 +0100

    update skill parser

--- src/main/java/com/example/ocrtest/services/sectionImpl/SkillParser.java
+++ src/main/java/com/example/ocrtest/services/sectionImpl/SkillParser.java
@@ -13,13 +13,29 @@ public class SkillParser extends SectionParser {
     @Override
     public void parse(Section section, CV cv){
         List<Skill> skillslist = new ArrayList<>();
+        String [] domain = null;
+        int i=0;
         try {
             for(String line:section.getContent()){
-                String [] domainskills = line.trim().split(":");
-                String [] skills = domainskills[1].split(",");
-                for(String skill:skills){
-                    Skill ski = new Skill(skill.trim(),domainskills[0]);
-                    skillslist.add(ski);
+                if(line.split(":").length>1) {
+                    String[] domainskills = line.trim().split(":");
+                    String[] skills = domainskills[1].split(",");
+                    for (String skill : skills) {
+                        Skill ski = new Skill(skill.trim(), domainskills[0]);
+                        skillslist.add(ski);
+                    }
+                }else {
+                    if(i==0) {
+                        domain = line.trim().split(":");
+                    }else if(i==1){
+                        String [] skills = line.trim().split(",");
+                        for(String skill:skills){
+                            Skill sky = new Skill(skill.trim(),domain[0]);
+                            skillslist.add(sky);
+                        }
+                        i=-1;
+                    }
+                    i++;
                 }
             }
             cv.setSkills(skillslist);
