33284b659715d7c89c4ed1193c5850e972bdfeb6
Bader Toumi <55552093+bader-t@users.noreply.github.com>
2022-08-22 11:29:44 +0100

    edited personal parser

--- src/main/java/com/example/ocrtest/services/sectionImpl/PersonalParser.java
+++ src/main/java/com/example/ocrtest/services/sectionImpl/PersonalParser.java
@@ -14,32 +14,12 @@ public class PersonalParser extends SectionParser {
             String phoneregex = "(\\+212|0)([ \\-_/]*)(\\d[ \\-_/]*){9}";
             Pattern emailpattern = Pattern.compile(emailregex);
             Pattern phonepattern = Pattern.compile(phoneregex);
-            int i = 0;
             for (String line : section.getContent()) {
                 if (emailpattern.matcher(line.trim()).find()) {
                     cv.setEmail(line.trim());
                 } else if (phonepattern.matcher(line.trim()).find()) {
                     cv.setPhoneNumber(line.trim());
-                } else if (i == 0 || i == 1) {
-                    String[] firstlast = line.trim().split(" ");
-                    if (firstlast.length > 1) {
-                        if (firstlast[0].trim().length() > 1) {
-                            cv.setFirstName(firstlast[1].trim());
-                            cv.setLastName(firstlast[0].trim());
-                        } else if (firstlast[0].trim().length() == 1) {
-                            firstlast = line.trim().split("  ");
-                            cv.setFirstName(firstlast[1].trim());
-                            cv.setLastName(firstlast[0].trim());
-                        }
-                        i = 5;
-                    } else {
-                        if (i == 0)
-                            cv.setLastName(line.trim());
-                        else if (i == 1)
-                            cv.setFirstName(line.trim());
-                    }
                 }
-                ++i;
             }
     }
 }
